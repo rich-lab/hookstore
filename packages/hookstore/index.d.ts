55db077d888797abe82f4dcceab959659fc8a350
chemdemo <yangdemo@gmail.com>
2019-11-09 18:04:34 +0800

    refactor: interface -> type

--- packages/hookstore/index.d.ts
+++ packages/hookstore/index.d.ts
@@ -5,31 +5,25 @@ export type State = {};
 
 export type ActionFn = () => any | Promise<any>;
 
-export interface Model {
+export type Model = {
   readonly name: string, // name of model
   state?: State, // model state
   actions: {
     [action: string]: ActionFn,
-  },
-  // middlewares?: Middleware[],
+  }
 }
 
 // export type BoundAction = (this: { ctx: Context }, ...args: any[]) => Promise<any>;
 export type BoundAction = (...args: any[]) => Promise<any>;
 
-export interface Actions {
+export type Actions = {
   [ action: string ]: BoundAction
 }
 
 // selector(state => state.count);
-export type StateSelector<S = State> = (state: S) => any;
+export type Selector<S = State> = (state: S) => any;
 
-// export interface Store<S = State> {
-//   getState<O extends any>(selector?: StateSelector<S, O>): O;
-//   actions: Actions;
-// }
-
-export interface Context<S = State> {
+export type Context<S = {}> = {
   // access current store's name
   readonly name: string,
   // access current action's name
@@ -39,18 +33,14 @@ export interface Context<S = State> {
   // access the bound action collection of current store
   actions: Actions,
   // access the lastest state and actions of some other store
-  getStore: (name?: string, selector?: StateSelector<S>) => [ any, Actions ],
-  // getStore: (name?: string) => Store,
-  // getState: <V extends any>(name?: string, selector?: StateSelector<S, V>) => V,
+  getStore: (name?: string, selector?: Selector<S>) => [ any, Actions ],
   // fresh the changed state to DOM(inside action handler) before action run finished
   fresh: (msg?: string) => void,
 }
 
 export type Next = () => Promise<any>;
 
-export interface Middleware {
-  (ctx: Context, next: Next): Promise<any>;
-}
+export type Middleware = (ctx: Context, next: Next) => Promise<any>;
 
 export type ProviderProps = {
   model?: Model,
@@ -61,16 +51,15 @@ export const Provider: React.FC<PropsWithChildren<ProviderProps>>;
 
 export function useStore<S = State>(
   name: string,
-  selector?: StateSelector<S>,
+  selector?: Selector<S>,
+  equalityFn?: (a: any, b: any) => boolean
 ): [ any, Actions ];
 
-export interface ActionStatus {
+export function useStatus(actionWithName: string): {
   error: null | Error,
   pending: boolean,
-}
+};
 
-export function useStatus(actionWithName: string): ActionStatus;
-
-export function getStore<S = State>(name: string, selector?: StateSelector<S>): [ any, Actions ];
+export function getStore<S = State>(name: string, selector?: Selector<S>): [ any, Actions ];
 
 export function applyMiddlewares(middlewares: Middleware[]): void;
