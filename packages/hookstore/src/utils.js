c7e588834db9e063817173bb966f55c093c2a335
chemdemo <yangdemo@gmail.com>
2019-11-09 14:41:09 +0800

    chore: lint

--- packages/hookstore/src/utils.js
+++ packages/hookstore/src/utils.js
@@ -100,9 +100,9 @@ const hasOwn = Object.prototype.hasOwnProperty;
 function is(x, y) {
   if (x === y) {
     return x !== 0 || y !== 0 || 1 / x === 1 / y;
-  } else {
-    return x !== x && y !== y;
   }
+
+  return x !== x && y !== y;
 }
 
 // copy from react-redux
